269835f41d7489bd5d3a014f04a941bd37397829
Harry Bloch <harryfbloch@gmail.com>
2018-10-05 14:54:33 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,7 @@
 class User 
   attr_accessor :first_name, :last_name
   
-  def initialize(first_name, last_name)
-    self.first_name = first_name
-    self.last_name = last_name
+  def initialize
+
   end
 end
